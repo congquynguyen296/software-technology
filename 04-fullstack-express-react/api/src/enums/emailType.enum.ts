28d252718f65ac86d69c3746a748dd77c63af344
congquynguyen296 <congquynguyen296@gmail.com>
2025-09-09 08:59:57 +0700

    FuzzySearch and Filter

--- 04-fullstack-express-react/api/src/enums/emailType.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum EmailTypeEnum {
-  VERIFY_EMAIL = 'verify-email',
-  FORGOT_PASSWORD_OTP = 'FORGOT_PASSWORD_OTP'
-}
--- 04-fullstack-express-react/api/src/index.ts
+++ 04-fullstack-express-react/api/src/index.ts
@@ -1,8 +1,8 @@
-import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import { connectDB } from './config/db';
-import authRoutes from './routes/auth.routes';
+import connectDB from './config/db';
+import productRoutes from './routes/product.routes';
+import searchRoutes from './routes/search.routes';
 
 const app = express();
 app.use(cors());
@@ -10,9 +10,10 @@ app.use(express.json());
 
 app.get('/health', (_req, res) => res.json({ ok: true }));
 
-app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/search', searchRoutes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`API running http://localhost:${PORT}`));
 });
